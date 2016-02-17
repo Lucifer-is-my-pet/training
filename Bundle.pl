ecb0e3ac4197beb44a2ab3145904fbaa65a022a3
Lucifer-is-my-pet <dianka-piratka@list.ru>
2016-02-17 16:48:11 +0500

    Click-click теперь нерабочий, готов основной функционал bundle.pl

--- Bundle.pl
+++ Bundle.pl
@@ -1,30 +1,48 @@
 use strict;
 use warnings;
 use File::Find;
-#$\ = "\n";
 
-# @ARGV = qw(.) unless @ARGV;
-my @fileList = ();
-find (\&wanted, './js');
+my $compressor = 'c:\Downloads\yuicompressor-2.4.8\yuicompressor-2.4.8.jar';
 
-sub wanted {
-    if ($_ =~ /(.+\.js)/) {
-        push @fileList, $File::Find::name;
+sub concatAndCompress {
+    my $extension = shift;
+    my $bundledFile = 'bundle.' . $extension;
+    my @fileList = ();
+    find (sub {
+            if ($_ =~ /(.+\.$extension)/) {
+            push @fileList, $File::Find::name;
+        }
+        }, $extension);
+
+    my $out = '';
+
+    for my $file(@fileList) {
+        open my $text, "<", $file or die "Could not open file $file : $!";
+
+        while (my $line = <$text>) {
+          $out .= $line;
+        }
+        $out .= "\n";
     }
-}; # /(.+\.txt)/ && print
 
-#print(join ", ", @fileList);
-my $out = '';
+    open my $output, '>', $bundledFile;
+    print {$output} $out;
 
-for my $file(@fileList) {
-#    open OUTPUT, ">>", 'bundle.js';
-    open my $text, "<", $file or die "Could not open file $file : $!";
-
-    while (my $line = <$text>) {
-      $out .= $line;
-    }
-    $out .= "\n";
+    my $compressed = `java -jar $compressor $bundledFile`;
+    return $compressed;
 }
 
-open OUTPUT, '>', 'bundle.js';
-print OUTPUT $out;
+my $compressedJS = concatAndCompress('js');
+my $compressedCSS = concatAndCompress('css');
+
+local $/ = undef;
+
+open my $input, '<', 'Click-click.html' or die "Could not open file 'Click-click.html' : $!";
+open my $output, '>', 'bundle.html';
+
+my $html = <$input>;
+$html =~ s%(.*<script.+/script>\n|.*<link.+>\n)%%mg;
+my $style = "<style>" . $compressedCSS . "</style>\n";
+my $script = "<script>" . $compressedJS . "</script>\n";
+$html =~s%</head>%$style$script</head>%;
+print {$output} $html;
---
+++ js/Main.js
@@ -0,0 +1,56 @@
+reactOnHashChange();
+
+function reactOnHashChange () {
+    var hash = location.hash.substr(1);
+    addDataToDiv(getUrl());
+
+    var links = document.querySelectorAll('.link');
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        link.style.fontWeight = '';
+        link.onclick = null;
+
+        if (link.getAttribute('href').substr(1) == hash) {
+            link.style.fontWeight = 'bold';
+            link.onclick = reactOnHashChange;
+        }
+    }
+}
+
+function addDataToDiv (url) {
+    getData(url, function(json) {
+        var jsonData = json['Data'] || '';
+        getById('addData').innerHTML = jsonData;
+        if (jsonData != '') {
+            getById('addData').innerHTML +=
+                '<br><a id="change" href="/">Изменить</a>';
+            getById('change').onclick = reactOnDataChange;
+        }
+    });
+}
+
+function reactOnDataChange (event) {
+    event.preventDefault();
+    var newDialog = createDialog({Title: 'Изменить', OkEvent: getInput});
+}
+
+function getInput(event) {
+    var dialog = event.target.closest('.dialog');
+    var newData = dialog.querySelector('input').value;
+    if (newData) {
+        var url = getUrl() + '&value=' + encodeURIComponent(newData);
+        getData(url, function (json) {
+            if (json['Result'] == 'OK') {
+                reactOnHashChange();
+            }
+        });
+    }
+    destroyDialog(dialog);
+}
+
+//вспомогательные функции
+function getUrl () {
+    return 'http://78.47.75.106/e/test&par=' + location.hash.substr(1);
+}
+
+onhashchange = reactOnHashChange;
